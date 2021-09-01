3697ac75caaee0de0fbba744f188d12ab9f4659b
bertekg <bartek.grywalski@gmail.com>
2021-09-01 21:38:52 +0200

    Add first part of game control and game logic
    It is possible only move frame. Create game logic script with correct random number selection.

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,20 @@ namespace CodeBreaker_MonoGame
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        SpriteFont gameFont;
+        SpriteFont debugFont;
+        Texture2D frameSprite;
+        int frameIndex = 0;
+        float framePosition = 0.0f;
+
+        bool rightReleased = true;
+        bool leftReleased = true;
+        bool upRelesed = true;
+        bool downRelesed = true;
+
+        int codeLength = 4;
+        GameLogic gameLogic;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -18,7 +32,7 @@ namespace CodeBreaker_MonoGame
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            gameLogic = new GameLogic(codeLength);
 
             base.Initialize();
         }
@@ -27,15 +41,73 @@ namespace CodeBreaker_MonoGame
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            gameFont = Content.Load<SpriteFont>("spaceFont");
+            debugFont = Content.Load<SpriteFont>("debugFont");
+            frameSprite = Content.Load<Texture2D>("frame");
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            KeyboardState keyboardState = Keyboard.GetState();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || keyboardState.IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if(keyboardState.IsKeyDown(Keys.Right) && rightReleased == true)
+            {
+                frameIndex++;
+                if (frameIndex > codeLength - 1)
+                {
+                    frameIndex = 0;
+                }
+                rightReleased = false;
+            }
+            else if(keyboardState.IsKeyUp(Keys.Right) && rightReleased == false)
+            {
+                rightReleased = true;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Left) && leftReleased == true)
+            {
+                frameIndex--;
+                if (frameIndex < 0)
+                {
+                    frameIndex = codeLength - 1;
+                }
+                leftReleased = false;
+            }
+            else if (keyboardState.IsKeyUp(Keys.Left) && leftReleased == false)
+            {
+                leftReleased = true;
+            }
+            framePosition = 85.0f + (frameIndex * 100.0f);
+
+            if (keyboardState.IsKeyDown(Keys.Up) && upRelesed == true)
+            {
+                gameLogic.currentCode[frameIndex]++;
+                if (gameLogic.currentCode[frameIndex] > 9)
+                {
+                    gameLogic.currentCode[frameIndex] = 0;
+                }
+                upRelesed = false;
+            }
+            else if (keyboardState.IsKeyUp(Keys.Up) && upRelesed == false)
+            {
+                upRelesed = true;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Down) && downRelesed == true)
+            {
+                gameLogic.currentCode[frameIndex]--;
+                if (gameLogic.currentCode[frameIndex] < 0)
+                {
+                    gameLogic.currentCode[frameIndex] = 9;
+                }
+                downRelesed = false;
+            }
+            else if (keyboardState.IsKeyUp(Keys.Down) && downRelesed == false)
+            {
+                downRelesed = true;
+            }
 
             base.Update(gameTime);
         }
@@ -44,7 +116,15 @@ namespace CodeBreaker_MonoGame
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            for (int i = 0; i < codeLength; i++)
+            {
+                _spriteBatch.DrawString(gameFont, gameLogic.currentCode[i].ToString(), new Vector2(100 + i * 100, 100), Color.White);
+            }
+            _spriteBatch.Draw(frameSprite, new Vector2(framePosition, 100), Color.White);
+            _spriteBatch.DrawString(debugFont, "Frame idex: " + frameIndex.ToString() + ", Frame pos: " + framePosition.ToString(), new Vector2(3, 3), Color.White);            
+            _spriteBatch.DrawString(debugFont, "Current code: " + gameLogic.CurrentCodeString(), new Vector2(3, 25), Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ GameLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeBreaker_MonoGame
+{
+    public class GameLogic
+    {
+        public int[] goodCode { get; set; }
+        public int[] currentCode { get; set; }
+        public GameLogic(int codeLength)
+        {
+            RandomCode(codeLength);
+        }
+        public void RandomCode(int codeLength)
+        {
+            goodCode = new int[codeLength];
+            List<int> possibleDigits = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                possibleDigits.Add(i);
+            }
+            Random random = new Random();
+            int indexDigit;
+            for (int i = 0; i < goodCode.Length; i++)
+            {
+                indexDigit = random.Next(0, possibleDigits.Count);
+                goodCode[i] = possibleDigits[indexDigit];
+                possibleDigits.RemoveAt(indexDigit);
+            }
+            currentCode = new int[codeLength];
+        }
+        public string CurrentCodeString()
+        {
+            string currentCodeString = "";
+            for (int i = 0; i < goodCode.Length; i++)
+            {
+                currentCodeString += "[" + goodCode[i].ToString() + "] ";
+            }
+            return currentCodeString;
+        }
+    }
+}
